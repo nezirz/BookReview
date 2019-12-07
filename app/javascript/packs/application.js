5cfdac7e94b0d254bc4950b26ddaa44d5ab960e0
nezir <nezir.zahirovic@gmail.com>
2019-12-07 14:24:06 +0100

    Fixing dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,19 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-require("@rails/activestorage").start()
 require("channels")
-$('.dropdown - toggle').dropdown()
-console.log('webpacker is running')
+
+import "bootstrap";
+
+import $ from 'jquery';
+global.$ = jQuery;
+import 'easy-autocomplete/dist/jquery.easy-autocomplete';
+
+
+document.addEventListener("turbolinks:load",()=>{
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
